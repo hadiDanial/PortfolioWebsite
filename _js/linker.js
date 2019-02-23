e0130c7ee1f775d214b5088469de7c026794993a
hadiDanial <hadidanial97@gmail.com>
2019-02-24 01:49:31 +0200

    Flexbox for post list
    Changed the link list tiles from using floats and margins to using display: flex + media query for the flex box

--- _js/linker.js
+++ _js/linker.js
@@ -12,15 +12,15 @@ function loadDoc() {
 
 function addToHTML(json){
     var ajax = document.getElementById("ajax");
-    ajax.style.display = "inline-block";
+    //ajax.style.display = "inline-block";
     ajax.style.width="67%";
     
     for(var i = 0; i<json.length; i++){
         var div = document.createElement("div");
         div.classList = "tile";
-        div.style.width = "30%";
-        div.style.height = "250px";
-        div.style.margin = "1%";
+        //div.style.width = "30%";
+        //div.style.height = "250px";
+        //div.style.margin = "1%";
         ajax.appendChild(div);
         div.innerHTML += "<br><a style='text-decoration:underline; color:whitesmoke;' href='" +  json[i].path + "'> <h2>"+ json[i].title + "</h2></a>";
         div.innerHTML += "<p>Description: " + json[i].description + "</p>";
