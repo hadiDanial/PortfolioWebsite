4d864e6f144cc5bb8063880db0a35b4e76e53fba
hadiDanial <hadidanial97@gmail.com>
2019-04-03 00:30:51 +0300

    inDesign scripts post

--- blog/2019/3/indesign_scripts.md
+++ blog/2019/3/indesign_scripts.md
@@ -7,30 +7,30 @@
 
 #### *March 31st, 2019*
 
-I've been working with inDesign for several years now, and over time I've acquired a collection of useful scripts. Sometimes, though, you can't find what you want online, especially when it's a specific function that you want to use for a specific job. So, I made a few small scripts.
+I've been working with inDesign for several years now, and over time I've acquired a collection of useful scripts. Most of the time, I could find a way to accomplish what I needed to do, and sometimes I had to modify an existing script. A few times, though, I couldn't find anything, or I needed a specific task done, and so made my own scripts.
+
+These were only used and tested with CS6.
+
+I'll update this page if I create more scripts.
+
+To download, click the download link, then right-click the page and select the `Save as...` option.
+
+### BestFit
+
+This script lets you choose a `PDF` file and select a size and final page count, and it fills the page with that file in a grid (gangup-repeat). The document values are hard coded in, as are the auto export location and the placed document's dimensions, so you'll have to go in and change those manually if you need to use this. In my case, those values never changed for the project, so I didn't need to figure out how to get the UI input to work.
+
+Due to the specific nature of the job, I also didn't take orientation into account, so it doesn't find the best fit for given page size, it finds the best fit for the page size on the given orientation.
+
+This script was made during the local elections, when I had to repeat this process dozens of time, and while I could use the impose software on the printer, it was time consuming to then export the files individually to another computer, not to mention error prone. I wrote this script, and it saved a lot of time, as it would quickly run and export everything neatly.
+
+I got the `getPDFPageCount` and `macFileFilter` functions from somewhere else online.
+
+Best Fit: [Download](indesign_scripts_assets/BestFit.jsx)
 
 ### Rotate Objects
 
-These scripts were made when I had a large collection of images in a document and I needed to rotate them all randomly. I didn't feel like going through the 50+ images and rotating them manually, so I made this simple script. 
+These scripts were made when I had a large collection of images in a document and I needed to rotate them all randomly. I didn't feel like going through the 50+ images and rotating them manually, so I made these simple scripts. These rotate all selected (active) objects. You can easily change the values to whatever you want, they're pretty low as I wanted a more subtle effect.
 
-Rotate Objects Randomly: [Download](indesign_scripts/Rotate Objects Randomly.jsx)
+Rotate Objects Randomly: [Download](indesign_scripts_assets/RotateObjectsRandomly.jsx)
 
-### The Snake
-
-After the `Grid` system was done, it was time to create the `Snake` and define how it moves. The `Snake` script was responsible for snapping the snake to the grid, moving it on the grid/2D array, rotating the head, and growing and shrinking it.
-
-The `SnakeController` script took in the player's input, and checked if the snake could move in that direction on the grid. If it could, it send instructions to the `Snake` script to move the head and update all the body parts.
-
-### The Art
-
-![Snakescape Art](snakescape_assets/tiles.png)
-
-We didn't have an artist working with us on this project, and we didn't even have any decent placeholder art. So, on the last day, I opened up Affinity Designer and started working. I'm not an artist, but I know my way around design, and considering the game was based on tiles, I managed to create all the art for the game in ~6 hours. I'm actually happy with how it turned out, and it gave me some confidence regarding my abilities.
-
-### Looking Back
-
-Once again, as it is a jam game, there's a lot that could be improved upon.
-
-1. The level editing was a pain, and it really needs to be made more user friendly. I wasn't aware of Unity's Tilemap system at the time of this jam, but maybe that would have helped, and saved us a lot of time and trouble creating the custom grid system and manually creating the levels.
-
-2. The gameplay turned out to be fine. There were no major bugs, so that's good, but we should have made a *lot* of extra levels. The ones we had were good, but the game ended up being too short.
+Set Rotation to zero: [Download](indesign_scripts_assets/SetRotationToZero.jsx)
---
+++ blog/2019/3/indesign_scripts_assets/BestFit.jsx
@@ -0,0 +1,252 @@
+﻿// This script takes a pdf file and repeats the page over the document in a grid.
+//If you want to change this to your own values, you can change the preset document sizes, the units, and the distance between the repeats. I added comments where things need changing.
+
+var myDocument;
+var tempObjStyle;
+var theFile;
+var nameAddition;
+var pages;
+var dialog;
+var rb4, rb5, rb6, rb7, rb8;
+var UIresult = myDisplayDialog();  
+var ind = 0;
+
+// Document size presets
+if (UIresult.rb[0]){nameAddition = "_21x30"; createDocument("210", "300");}  
+if (UIresult.rb[1]){nameAddition = "_30x42"; createDocument("420", "300");}  
+if (UIresult.rb[2]){nameAddition = "_A4"; createDocument("297", "210");}  
+if (UIresult.rb[3]){pages = 1; }
+if (UIresult.rb[5]){pages = 50; }
+if (UIresult.rb[6]){pages = 100; }
+
+function createDocument(docWidth,docHeight)  
+{  
+    app.pdfPlacePreferences.pdfCrop = PDFCrop.cropMedia;
+    if(multiPage.value)
+    {
+        createDocuments(docWidth,docHeight)  ;
+    }
+    else 
+    {
+        // Change units here
+        myDocument = app.documents.add();  
+        with(myDocument.documentPreferences){  
+            pageHeight = docHeight + "mm";  
+            pageWidth = docWidth + "mm";  
+            pagesPerDocument = 1;}  
+            ind = 1;
+            placePage();
+        }
+    }
+    
+    function createDocuments(docWidth,docHeight)  
+    {  
+        ind = 1;
+        var num = getPDFPageCount(theFile);
+        for(var i=1; i<=  num; i++)
+        {
+            ind = i;
+            app.pdfPlacePreferences.pageNumber = ind;
+            myDocument = app.documents.add();  
+            // Change units here
+            with(myDocument.documentPreferences){  
+                pageHeight = docHeight + "mm";  
+                pageWidth = docWidth + "mm";  
+                pagesPerDocument = 1;}  
+                placePage();
+                
+            }
+            
+        }
+        
+        function placePage()  
+        {
+            
+            if(useMaster.value == true)
+            {
+                PlacePage(myDocument.masterSpreads.item(0).pages[0]);
+            }
+            else
+            {
+                PlacePage(myDocument.pages[0]);
+            }
+            
+        }
+        
+        function PlacePage(myPage)
+        {
+            var w = myDocument.documentPreferences.pageWidth;
+            var h = myDocument.documentPreferences.pageHeight;
+            
+            // Change width and height here.
+            var pdfW = 70;
+            var pdfH = 100;
+            
+            //Number of repeats
+            var x = Math.floor(w/pdfW);
+            var y = Math.floor (h/pdfH);
+            
+            var initX = (w - (x * pdfW))/2;
+            var initY = (h - (y * pdfH))/2;
+            
+            if(rb4.value == true)
+            {
+                pages = 1;
+            } else if(rb4.value == true)
+            {
+                pages = 25;
+            } else if(rb5.value == true)
+            {
+                pages = 50;
+            } else if(rb6.value == true)
+            {
+                pages = 100;
+            }
+            
+            for(var i = 2; i <= pages; i++)
+            myDocument.pages.add();
+            
+            
+            for(var i = 0; i<x; i++)
+            {
+                for(var j = 0; j<y; j++)
+                {
+                    app.pdfPlacePreferences.pageNumber = ind;
+                    var myPDFPage = myPage.place(File(theFile), [initX + i * pdfW, initY + j*pdfH],myDocument.activeLayer,false)[0];
+                }
+            }
+            var myObj = new Array;
+            myObj = app.activeWindow.activePage.pageItems;
+            
+            if(exportCheck.value)
+            ExportPDF();
+        }
+        
+        
+        function ExportPDF()
+        {
+            //Change auto-export directory here
+            app.activeDocument.exportFile(ExportFormat.pdfType, File("/Users/admin/Desktop/BestFit/"+theFile.name+nameAddition + "_" + ind + ".pdf"), false);
+            app.activeDocument.close(SaveOptions.no);
+        }
+        
+        
+        
+        function myDisplayDialog()  
+        {  
+            // Ask user to select the PDF/InDesign file to place
+            var askIt = "Select a PDF or InDesign file to place:";
+            if (File.fs =="Windows")
+            {
+                theFile = File.openDialog(askIt, "Placeable: *.indd;*.pdf");
+            }
+            else if (File.fs == "Macintosh")
+            {
+                theFile = File.openDialog(askIt, macFileFilter);
+            }
+            else
+            {
+                theFile = File.openDialog(askIt);
+            }
+            var myDialog = new Window ('dialog', 'Elections - Choose Size');  
+            myDialog.alignChildren = "left";  
+            var rg0 = myDialog.add ('group');  
+            rg0.add('statictext',undefined, 'Select Size:');  
+            // Change labels for sizes here.
+            var rb1 = rg0.add('radiobutton',undefined, '21x30');  
+            var rb2 = rg0.add('radiobutton',undefined, '30x42');  
+            var rb3 = rg0.add('radiobutton',undefined, 'A4');  
+            rg0.add('statictext',undefined, 'mm');  
+            rb2.value = true;  
+            var rg1 = myDialog.add ('group');  
+            var rg3 = myDialog.add ('group');  
+            var rg2 = myDialog.add ('group');  
+            rg3.alignment="left";
+            rg3.add('statictext',undefined, 'Select Quantity:');  
+            rb4 = rg3.add('radiobutton',undefined, '1');  
+            rb5 = rg3.add('radiobutton',undefined, '50');  
+            rb6 = rg3.add('radiobutton',undefined, '100'); 
+            rb6.value = true;
+            useMaster = rg2.add('checkbox', undefined,'Place on Master');
+            useMaster.value = true;
+            exportCheck = rg2.add('checkbox', undefined,'Auto Export and Close');
+            exportCheck.value = true;
+            multiPage = rg2.add('checkbox', undefined,'Multipage PDF');
+            multiPage.value = true;
+            //duplex = rg2.add('checkbox', undefined, 'Duplex?');
+            var end = myDialog.add('group');
+            end.add('statictext',undefined, 'By Hadi'); 
+            end.alignment = "right";  
+            end.add('button', undefined, 'Cancel', {name: 'cancel'});  
+            end.add('button', undefined, 'OK', {name: 'ok'});  
+            
+            var myResult = myDialog.show();  
+            dialog = myDialog;
+            if (myResult == 1)  
+            {  
+                return {rb : [rb1.value, rb2.value, rb3.value, rb4.value, rb5.value, rb6.value]};  
+            }  
+            if (myResult == 2)  
+            {  
+                exit();  
+            }  
+            //PlacePage();
+        }  
+        
+        
+        // File filter for the mac to only show indy and pdf files
+        function macFileFilter(fileToTest)
+        { 
+            if(fileToTest.name.indexOf(".pdf") > -1 || fileToTest.name.indexOf(".ind") > -1)
+            return true; 		 
+            else
+            return false;	 
+        }
+        
+        
+        function getPDFPageCount(f) { 
+            
+            
+            f.open ('r');
+            
+            var gotCount = false;
+            
+            while (! gotCount) {
+                
+                next_line = f.readln();
+                
+                if  ( f.eof ) {alert("Aborting the script\nWe've got to the end of the file without finding a page count");
+                
+                f.close();
+                
+                exit();
+                
+            }
+            
+            if (next_line.indexOf ('/N ') > 0) { // We've got the easy sort of PDF
+            
+            var p = next_line.match (/\/N (\d+)\/T/)[1];
+            
+            //alert("Found a '/N' style PDF, with "+p+" pages");
+            
+            gotCount = true;
+            
+        }
+        
+        else if  (next_line.indexOf ('/Pages>>') > 0 ) {  // We probably had to read nearly to the end of the file for the match...
+            
+            var p = next_line.match (/\/Count (\d+)\/K/)[1];
+            
+            //alert("Found a '/Count ... /Pages>>' style PDF, with "+p+" pages");
+            
+            gotCount = true;
+            
+        }
+        
+    }
+    
+    f.close ();
+    
+    return Number(p);
+    
+}
