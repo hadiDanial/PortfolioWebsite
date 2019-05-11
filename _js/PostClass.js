6ad1956cc02441b3a7f637b8fa6f065c5dfc2809
hadiDanial <hadidanial97@gmail.com>
2019-05-11 23:11:31 +0300

    Update PostClass.js
    Added function to create a post div with a <picture> tag, but not using it after all

--- _js/PostClass.js
+++ _js/PostClass.js
@@ -53,6 +53,48 @@ class Post {
         div.append(text);
         return div;
     }
+
+    /**
+    * Creates a div containing the relevant info from a Post object, with a <picture> tag that dynamically changes images based on media query size.
+    * @param {bool} addTags Add the post tags to the div?.
+    * @param {number} i Index of the post. Used for assigning id.
+    */
+    BuildPostDivPicTag(i, addTags = false) {
+        var div = document.createElement("div");
+        var img = document.createElement("img");
+        var pic;
+        
+        var src1 = "<source media='(max-width: 400px)' srcset='/_img/regular.png'>";
+        var src2 = "<source media='(max-width: 900px)' srcset='/_img/regular.png'>";
+        var text = document.createElement("text");
+        var id = "link-" + i;
+
+        img.src = "/_img/regular.png";
+        //img.src = this.coverImage;
+        img.onerror = function(){
+            img.src = "/_img/defaultCover.png";
+        }
+        var p = this.path;
+        img.onclick = function() {
+            window.location.href = p;
+        };
+        //img.width = "40%";
+        text.innerHTML += "<br><a href='" + this.path + "' style='text-decoration:underline; color:whitesmoke;' id='" + id + "' > <h3>" + this.title + "</h3></a>"; //onclick='loadLink("+i+")'
+        text.innerHTML += "<p>" + this.description + "</p>";
+        text.innerHTML += "<p><em>" + this.date + "</em></p>";
+        var t = "";
+        if (addTags) {
+            t = this.GetTags();
+        }
+        text.innerHTML += t;
+        text.classList.add("tileText");
+        text.innerHTML += "<br>";
+        pic = "<picture> " + src1+ src2+ img.outerHTML + "</picture>";
+        div.innerHTML += pic;
+        div.append(text);
+        return div;
+    }
+
     /**
     * Returns the tags of the post formatted as a string in <p> and <em> tags.
     */
